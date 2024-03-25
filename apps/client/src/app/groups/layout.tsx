795b31c86be8f5e0e79da07bd154916818bec1af
belucent0 <belucent0@gmail.com>
2024-03-25 23:46:29 +0900

    [client-groups] 그룹 목록 페이지, 그룹 생성 버튼 추가

---
+++ apps/client/src/app/groups/layout.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: "그룹| CRM",
+    description: "그룹 관리 페이지입니다.",
+};
+
+const GroupsLayout = ({ children }: { children: React.ReactNode }) => {
+    return <>{children}</>;
+};
+
+export default GroupsLayout;
--- apps/client/src/app/groups/page.tsx
+++ apps/client/src/app/groups/page.tsx
@@ -9,12 +9,14 @@ import {
 } from "@/components/ui/pagination";
 import { Payment, columns } from "./columns";
 import { DataTable } from "./data-table";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContainer, CardContent } from "@/components/ui/card";
 import { Session, getServerSession } from "next-auth";
 import { authOptions } from "../../../pages/api/auth/[...nextauth]";
 import { getData } from "./data";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export default async function DemoPage() {
+export default async function GroupsPage() {
     // const session: Session | null = await getServerSession(authOptions);
 
     // const jwt = session?.accessToken;
@@ -32,24 +34,41 @@ export default async function DemoPage() {
     const data = await getData();
 
     return (
-        <div className="container mx-auto py-10">
-            <DataTable columns={columns} data={data} />
-            <Pagination>
-                <PaginationContent>
-                    <PaginationItem>
-                        <PaginationPrevious href="#" />
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink href="#">1</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationNext href="#" />
-                    </PaginationItem>
-                </PaginationContent>
-            </Pagination>
-        </div>
+        <>
+            <div className="container mx-auto py-10">
+                <Card>
+                    <CardContainer>
+                        <CardContent>
+                            <div className="flex justify-between items-center">
+                                <h1 className="text-2xl font-semibold">그룹 목록</h1>
+                                <Link href="/groups/produce">
+                                    <Button>새 그룹 생성하기</Button>
+                                </Link>
+                            </div>
+                        </CardContent>
+
+                        <CardContent>
+                            <DataTable columns={columns} data={data} />
+                            <Pagination>
+                                <PaginationContent>
+                                    <PaginationItem>
+                                        <PaginationPrevious href="#" />
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationLink href="#">1</PaginationLink>
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationEllipsis />
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationNext href="#" />
+                                    </PaginationItem>
+                                </PaginationContent>
+                            </Pagination>
+                        </CardContent>
+                    </CardContainer>
+                </Card>
+            </div>
+        </>
     );
 }
